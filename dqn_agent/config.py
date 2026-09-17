9baf92528ec3c6cc46bb4eccab23cb7f068d041e
mhaham786 <mhaham786@gmail.com>
2026-09-17 10:34:09 +0200

    dqn task4

--- dqn_agent/config.py
+++ dqn_agent/config.py
@@ -8,20 +8,20 @@ EXPERIMENTS_DIR = AGENT_DIR / "experiments"
 
 ACTIONS = ("UP", "RIGHT", "DOWN", "LEFT", "WAIT", "BOMB")
 
-EXPERIMENT_NAME = "dqn_task3_op"
-DESCRIPTION = "Task 3 safe offensive bombing and pre-emptive safety."
+EXPERIMENT_NAME = "task4_3_rule_based_v1"
+DESCRIPTION = "Task 4 against 3 rule based agents"
 
 RESUME_TRAINING = False
 RESTART_EXPERIMENT = False
-INITIAL_WEIGHTS_EXPERIMENT = None
+INITIAL_WEIGHTS_EXPERIMENT = "dqn_task3_op"
 
 OBSERVATION_COUNT = 33
 
 # DQN
 GAMMA = 0.99
-LEARNING_RATE = 1e-4
+LEARNING_RATE = 5e-5 #reduced from 1e-4 to 5e-5
 TARGET_UPDATE_INTERVAL = 1_000
-EPSILON_START = 0.5
+EPSILON_START = 0.1  #reduced from 0.5 to 0.1
 EPSILON_END = 0.001
 EPSILON_DECAY_STEPS = 20_000
 GRADIENT_CLIP_NORM = 10.0
